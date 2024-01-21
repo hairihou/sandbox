42b71b33fc5dcf354b8a3065e07fad6897921055
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2024-01-22 02:18:41 +0900

    chore(): remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { unstable_vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [remix({ unstable_ssr: false }), tsconfigPaths()],
 });
